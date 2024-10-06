fc96a68b9f018db602942aedfa447c5c17966394
Hugo Esparza C <hugo.esparza3@gmail.com>
2024-10-06 16:33:05 -0600

    add: views/pages/index and PagesController.php

--- public/index.php
+++ public/index.php
@@ -3,8 +3,11 @@
 require_once __DIR__ . '/../includes/app.php';
 
 use MVC\Router;
+use Controllers\PagesController;
 
 $router = new Router();
+
+$router->get('/', [PagesController::class, 'index']);
 
 // Checks and validates the routes that exist and assigns them the Controller functions
 $router->checkRoutes();
