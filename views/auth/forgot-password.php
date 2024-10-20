5601ca4ab1f9bdfd1041fb4164462f161a8fb2ce
Hugo Esparza C <hugo.esparza3@gmail.com>
2024-10-19 20:04:32 -0600

    add: forgot password frontend

---
+++ views/auth/forgot-password.php
@@ -0,0 +1,24 @@
+<div class="phone-login"></div>
+<div class="login">
+    <form>
+        <h1>Recuperar cuenta</h1>
+        <div class="campo">
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mail" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <path d="M3 7a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v10a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-10z" />
+                <path d="M3 7l9 6l9 -6" />
+            </svg>
+            <input type="email" placeholder="Correo electrónico">
+        </div>
+        <div class="signup-link">
+            ¿No tienes una cuenta? <a href="signup">Regístrate</a>
+        </div>
+        <div class="signup-link">
+            ¿Ya tienes una cuenta? <a href="login">Iniciar sesión</a>
+        </div>
+        <div class="cont-center">
+            <input class="correo-button" type="submit" value="Enviar correo de recuperación"/>
+        </div>
+    </form>
+</div>
+<div class="phone-login"></div>
